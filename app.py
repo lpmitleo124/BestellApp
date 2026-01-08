c6f58835dd747ded01e57ed4399669b1e7a2228c
lpmitleo124 <120562996+lpmitleo124@users.noreply.github.com>
2026-01-08 16:36:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -135,8 +135,8 @@ def generate_invoice_pdf(cart, customer_name, team):
 
     story.append(Paragraph("<b>PayPal:</b> https://www.paypal.com/pool/9kwYdJ6jNv?sr=wccr", styles["Normal"]))
     story.append(Paragraph("<b>Verwendungszweck:</b> Name + Team", styles["Normal"]))
-	story.append(Paragraph("<b>Banküberweisung</b> Leonard Kötter IBAN: DE03 4007 0224 0667 3586 01", styles["Normal"]))
-	story.append(Paragraph("<b>Verwendungszweck:</b> Name + Team", styles["Normal"]))
+		story.append(Paragraph("<b>Banküberweisung</b> Leonard Kötter IBAN: DE03 4007 0224 0667 3586 01", styles["Normal"]))
+		story.append(Paragraph("<b>Verwendungszweck:</b> Name + Team", styles["Normal"]))
     story.append(Paragraph("<b>Bei Problemen:</b> Leonard Kötter – 0173 6121352", styles["Normal"]))
 
     doc.build(story)
